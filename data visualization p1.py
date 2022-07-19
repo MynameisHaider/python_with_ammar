0a0dc4cf3f98c939acf55ab2d5708df410173901
Ali Haider <62500695+MynameisHaider@users.noreply.github.com>
2022-07-19 11:19:38 +0500

    add one comment at the end of code

--- data visualization p1.py
+++ data visualization p1.py
@@ -5,3 +5,4 @@ sns.set_theme(style="ticks",color_codes= True)
 titanic = sns.load_dataset("titanic")
 sns.catplot(x="sex", y="survived" , hue="class", kind="bar", data= titanic)
 plt.show()
+# add new comment
